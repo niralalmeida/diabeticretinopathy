4c4f5a45f6b8fa948f1b4762217634d3f73e8235
Rudolph Almeida <rudolf1.almeida@gmail.com>
2018-03-18 11:20:03 +0530

    Grid Search for Random Forest

--- random_forests.py
+++ random_forests.py
@@ -1,6 +1,8 @@
 """
  Random Forests Classifier for Diabetic Retinopathy Detection
 
+ Hyperparameter search using Grid Search
+
  Date: 19th February, 2018
 """
 
@@ -21,6 +23,29 @@ def main():
     lda.fit(train_x, train_y)
     train_x = lda.transform(train_x)
     test_x = lda.transform(test_x)
+
+    parameters = {
+        "n_estimators": [10, 50, 100, 500],
+        "criterion": ["gini", "entropy"],
+        "max_features": ["log2", "sqrt", "auto"],
+        "max_depth": [None, 10, 25, 50]
+    }
+
+    gd = GridSearchCV(RandomForestClassifier(), parameters, scoring="accuracy")
+
+    gd.fit(train_x, train_y)
+
+    print('Best Score: {}'.format(gd.best_score_))
+
+    print('Best Model:\n{}'.format(gd.best_estimator_))
+
+    rfc = gd.best_estimator_
+
+    pred = rfc.predict(test_x)
+
+    print('On Test Data...')
+
+    print('Accuracy: {}'.format(metrics.accuracy_score(test_y, pred)))
 
 
 if __name__ == '__main__':
