0eefa78a90d1b37d32370dd7988a5fd38f6bb9df
Rudolph Almeida <rudolf1.almeida@gmail.com>
2018-04-08 19:14:24 +0530

    Changed CNN to work with FloydHub and added results of ConvNet1

--- cnn/cnn1.py
+++ cnn/cnn1.py
@@ -1,18 +1,33 @@
 """
  Convolutional Neural Networks for Diabetic Retinopathy Detection
 
+ Will only run on floydhub.com on my diabret project
+
+ ConvNet 1 Results:
+ loss: 4.1806 - acc: 0.7406 - val_loss: 4.2411 - val_acc: 0.7369
+
+ Log file: cnn1.txt
+
+ Time taken: 41:28 Mins
+
+ GPU: NVidia Tesla K80 12GB
+
+ Total Data: 35125 images -> 5002 test, rest train
+
+ Attempts: 11 :)
+
  Date: April 3, 2018
 """
 
-from tensorflow.python.keras.layers import (Conv2D, Dense, Dropout, Flatten,
-                                            MaxPooling2D)
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from keras.layers import (Conv2D, Dense, Dropout, Flatten, MaxPooling2D)
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
 
 classifier = Sequential()
 
 # Architecture
-classifier.add(Conv2D(32, (5, 5), input_shape=(512, 512), activation='relu'))
+classifier.add(
+    Conv2D(32, (5, 5), input_shape=(512, 512, 1), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 classifier.add(Conv2D(64, (5, 5), activation='relu'))
@@ -33,25 +48,27 @@ classifier.add(Dropout(0.5))
 classifier.add(Dense(5, activation='softmax'))
 
 classifier.compile(
-    optimizer='adam', loss='catergorical_crossentropy', metrics=['accuracy'])
+    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-train_datagen = ImageDataGenerator(horizontal_flip=True)
+train_datagen = ImageDataGenerator()
 test_datagen = ImageDataGenerator()
 
 train_set = train_datagen.flow_from_directory(
-    './sample/train',
+    '/data/train',
     target_size=(512, 512),
+    color_mode="grayscale",
     batch_size=32,
     class_mode='categorical')
 test_set = test_datagen.flow_from_directory(
-    './sample/test',
+    '/data/test',
     target_size=(512, 512),
+    color_mode="grayscale",
     batch_size=32,
     class_mode='categorical')
 
 classifier.fit_generator(
     train_set,
     steps_per_epoch=100,
-    epochs=3,
+    epochs=20,
     validation_data=test_set,
     validation_steps=50)
